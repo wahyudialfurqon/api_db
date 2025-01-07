6a1d6e09fe90a8e3fce2e9af7aa20ea05b65edd8
wahyudialfurqon <w.alfurqon11@gmail.com>
2025-01-08 02:11:05 +0700

    k

---
+++ config/api.php
@@ -0,0 +1,9 @@
+'providers' => [
+    ...
+    CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
+],
+
+'aliases' => [
+    ...
+    'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
+],
